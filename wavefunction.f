fbbe827ad697a6fbf76fb1bfab18890572f48c3a
Ayurzana <ayur_426@yahoo.com>
2017-03-21 18:53:35 +0900

    Update wavefunction.f

--- wavefunction.f
+++ wavefunction.f
@@ -1,19 +1,22 @@
       program wavefunction
-      parameter (n=20, dt=0.01, pi=2.141592)
-      real phi(n), phi_0, nu, k, c, time
-      phi_0=1.0
-      nu=0.01
-      k=2.*pi
+      parameter (n=20, dt=0.01, pi=3.141592)
+      real phi(n)
+      aphi_0=1.0
+      anu=0.01
+      ak=2.*pi ! энэ хэмжигдэхүүнүүдийг өөрчилнө. 
       c=1.0
-      dx=1./n
-      time=5.0
-      iter=time/dt
-      do i=1,iter
-      t=i*dt
-        do j=1,n
-        x=j*dx
-        phi(j)=phi_0*exp(-nu*k**2*t)*cos(k*(x-c*t))
+      dx=1./n ! торын алхам
+      atime=5.0
+      iter=atime/dt
+
+      open(10,file='data.dat')
+      do i=0,iter
+        a=dt*i
+        do j=0,n
+        ax=j*dx
+        phi(j)=aphi_0*exp(-anu*ak**2*a)*cos(ak*(ax-c*a))
+      if(mod(i,100).eq.0) write(10,*) j*dx, phi(j)
         end do
-      end do
+      end do  
       stop
-      end program
+      end program  
